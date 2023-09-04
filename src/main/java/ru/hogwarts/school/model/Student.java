cb085c923427329c422fb2ff6603ea0aa4188530
Cephey <msvprosto@mail.ru>
2023-09-04 23:02:24 +0400

    Refactor

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.model;
 
+import java.util.Objects;
+
 public class Student {
     private Long id;
     private String name;
@@ -9,6 +11,19 @@ public class Student {
         this.id = id;
         this.name = name;
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
     }
 
     public Long getId() {
