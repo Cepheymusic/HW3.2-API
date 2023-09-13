7e55a13b3b4fe76c490bcf97b97aae2485eb6d75
Cephey <msvprosto@mail.ru>
2023-09-14 00:59:34 +0400

    Add test

--- src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
+++ src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -11,7 +11,9 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.FacultyRepository;
 import ru.hogwarts.school.repository.StudentRepository;
 
 import java.util.List;
@@ -26,12 +28,18 @@ public class StudentControllerTest {
     int port;
     @Autowired
     StudentRepository studentRepository;
+    @Autowired
+    FacultyRepository facultyRepository;
+
     @AfterEach
     void afterEach() {
         studentRepository.deleteAll();
 
     }
+
     Student student = new Student(1L, "Harry", 10);
+    Faculty faculty = new Faculty(1L, "Grif", "red");
+
     @Test
     void create__returnStatus200AndStudent() {
 
@@ -42,14 +50,34 @@ public class StudentControllerTest {
         assertEquals(student.getName(), studentResponseEntity.getBody().getName());
         assertEquals(student.getAge(), studentResponseEntity.getBody().getAge());
     }
+
     @Test
     void read_studentNotInDb_returnStatus200AndStudent() {
         ResponseEntity<String> studentResponseEntity = restTemplate.getForEntity(
                 "http://localhost:" + port + "/student/" +
-                student.getId(), String.class);
+                        student.getId(), String.class);
         assertEquals(HttpStatus.BAD_REQUEST, studentResponseEntity.getStatusCode());
         assertEquals("Студент не найден", studentResponseEntity.getBody());
     }
+//    @Test
+//    void update__returnStatus200AndStudent() {
+//
+//        ResponseEntity<Student> studentResponseEntity = restTemplate.put(
+//                "http://localhost:" + port + "/student", student, Student.class);
+//        assertEquals(HttpStatus.OK, studentResponseEntity.getStatusCode());
+//        assertEquals(student.getName(), studentResponseEntity.getBody().getName());
+//        assertEquals(student.getAge(), studentResponseEntity.getBody().getAge());     не работает
+//    }
+//    @Test
+//    void delete__returnStatus200AndStudent() {
+//
+//        ResponseEntity<Student> studentResponseEntity = restTemplate.delete(
+//                "http://localhost:" + port + "/student", student.getId(), ResponseEntity<Student> student);
+//        assertEquals(HttpStatus.OK, studentResponseEntity.getStatusCode());
+//        assertEquals(student.getName(), studentResponseEntity.getBody().getName());
+//        assertEquals(student.getAge(), studentResponseEntity.getBody().getAge());   и этот........
+//    }
+
 
     @Test
     void readAll__returnStatus200AndStudentList() {
@@ -58,8 +86,35 @@ public class StudentControllerTest {
                 "http://localhost:" + port + "/student/by-age/" + student.getAge(),
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<>() {});
+                new ParameterizedTypeReference<>() {
+                });
         assertEquals(HttpStatus.OK, exchange.getStatusCode());
         assertEquals(List.of(student), exchange.getBody());
     }
+
+//    @Test
+//    void readFacultyByStudentId__returnStatus200AndFaculty() {
+//        studentRepository.save(student);
+//        ResponseEntity<Faculty> exchange = restTemplate.exchange(
+//                "http://localhost:" + port + "/student/faculty/" + student.getId(),
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<>() {
+//                });
+//        assertEquals(HttpStatus.OK, exchange.getStatusCode());
+//        assertEquals(List.of(faculty), exchange.getBody());    //бесполезно
+//    }
+//    @Test
+//    void readAllByAgeBetween__returnStatus200AndStudentList() {
+//        studentRepository.save(student);
+//        ResponseEntity<List<Student>> exchange = restTemplate.exchange(
+//                "http://localhost:" + port + "/student/search/" + student.getAge(),
+//                HttpMethod.GET,
+//                null,
+//                new ParameterizedTypeReference<>() {
+//                });
+//        assertEquals(HttpStatus.OK, exchange.getStatusCode());
+//        assertEquals(List.of(student), exchange.getBody());    можно было не пытаться
+//
+//    }
 }
