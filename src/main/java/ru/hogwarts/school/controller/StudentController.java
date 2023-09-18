ec3cc1a51795136e696ecb05d14d496fd2201bc9
Cephey <msvprosto@mail.ru>
2023-09-18 22:42:38 +0400

    Add loggi and port

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.controller;
 
 import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.exceptions.StudentNotFoundException;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.StudentService;
--- src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/AvatarServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,7 @@ public class AvatarServiceImpl implements AvatarService {
     private final String avatarsDir;
     private final StudentService studentService;
     private final AvatarRepository avatarRepository;
+    private final Logger logger = LoggerFactory.getLogger(StudentServiceImpl.class);
 
     public AvatarServiceImpl(StudentService studentService, AvatarRepository avatarRepository, @Value("${path.to.avatars.folder}") String avatarsDir) {
         this.studentService = studentService;
@@ -31,6 +34,7 @@ public class AvatarServiceImpl implements AvatarService {
 
     @Override
     public void uploadAvatar(Long studentId, MultipartFile avatarFile) throws IOException {
+        logger.info("Был вызван метод uploadAvatar с данными: {}, {}", studentId, avatarFile);
         Student student = studentService.read(studentId);
         Path filePath = Path.of(avatarsDir, student.getId() + "." + getExtensions(avatarFile.getOriginalFilename()));
         Files.createDirectories(filePath.getParent());
