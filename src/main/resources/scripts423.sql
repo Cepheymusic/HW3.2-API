28dc0d183b4ce671473ae1f02b70a2d61a98ce13
Cephey <msvprosto@mail.ru>
2023-09-18 11:55:23 +0400

    Refactor scripts

--- src/main/resources/scripts423.sql
+++ src/main/resources/scripts423.sql
@@ -1,4 +1,4 @@
 select s.name, s.age, f.name
 from student s left join faculty f on s.faculty_id = f.id;
-select s.name a.data
+select s.name, a.data
 from student s inner join avatar a on s.id = a.student_id
